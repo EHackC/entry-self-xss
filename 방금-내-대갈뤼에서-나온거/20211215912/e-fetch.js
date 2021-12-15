cb99d9f6b5820ca923727dca477d63669850ed49
EHackC <89516941+EHackC@users.noreply.github.com>
2021-12-15 22:02:16 +0900

    Update e-fetch.js

--- 방금-내-대갈뤼에서-나온거/20211215912/e-fetch.js
+++ 방금-내-대갈뤼에서-나온거/20211215912/e-fetch.js
@@ -6,17 +6,27 @@
 ;var EF=(function(){
   "use strict";
   var _ef={};
-  //-------------------------------------
+  //-------------------------------------------------------------
   _ef.createCommunity=function(type,data) { 
     return createCommunity(type,data);
   };
   return _ef;
-  //-------------------------------------
+  //-------------------------------------------------------------
   function init(){
     console.log("\"매우 생즈하군..\"\n         -EHackC");
     return _ef;
   }
   function createCommunity(type,data){/*글 등록*/
-    return (obj==null||obj==undefined);
+    if(type==entrystory){
+      query="mutation CREATE_ENTRYSTORY($content:String $text:String $image:String $sticker:String $cursor:String){createEntryStory(content:$content text:$text image:$image sticker:$sticker cursor:$cursor){warning discuss{id title content seContent created commentsLength likesLength visit category prefix groupNotice user{id nickname username profileImage{id name label{ko en ja vn}filename imageType dimension{width height}trimmed{filename width height}}status{following follower}description role}images{filename imageUrl}progress thumbnail reply bestComment{id user{id nickname username profileImage{id name label{ko en ja vn}filename imageType dimension{width height}trimmed{filename width height}}status{following follower}description role}content created removed blamed commentsLength likesLength isLike hide image{id name label{ko en ja vn}filename imageType dimension{width height}trimmed{filename width height}}sticker{id name label{ko en ja vn}filename imageType dimension{width height}trimmed{filename width height}}}blamed}}}";
+    }else{
+      return "ERROR: 알수없는 게시물 타입입니다. 타입을 entrystory, qna, tips중에 하나로 정했는지 확인해주시기 바랍니다";
+    }
+    fetch("https://playentry.org/graphql",{
+      "headers":{"content-type": "application/json"},
+      "body": "{\"query\":\""+query+",\"variables\":"+JSON.stringify(data)+"}",
+      "method": "POST",
+    });
+    return;
   }
 })();
